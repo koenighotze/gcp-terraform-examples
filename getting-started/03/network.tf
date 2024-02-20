37dde18e3417a47dca86d31feb06865b8d982676
koenighotze <david.schmitz.privat@gmail.com>
2024-02-20 18:25:52 +0100

    Use secrets from repo

--- getting-started/03/network.tf
+++ getting-started/03/network.tf
@@ -4,8 +4,15 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "subnetwork"
-  ip_cidr_range = "10.0.0.1/16" # todo actually too large
-  network       = google_compute_network.vpc.id
-  stack_type    = "IPV4_ONLY"
+  name                     = "subnetwork"
+  ip_cidr_range            = "10.0.0.1/16" # todo actually too large
+  network                  = google_compute_network.vpc.id
+  stack_type               = "IPV4_ONLY"
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
