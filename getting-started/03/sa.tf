8bc56e92a6bd189891e25f53b43c65f78bcd1e59
koenighotze <david.schmitz.privat@gmail.com>
2024-02-21 11:26:27 +0100

    Add service account for vm

---
+++ getting-started/03/sa.tf
@@ -0,0 +1,18 @@
+resource "google_service_account" "instance_service_account" {
+  project      = var.project_id
+  account_id   = "instance-service-account"
+  display_name = "Instance Service Account"
+}
+
+resource "google_project_iam_binding" "iam_binding_project" {
+  project = var.project_id
+  for_each = toset([
+    "roles/logging.logWriter",
+  ])
+
+  role = each.key
+
+  members = [
+    "serviceAccount:${google_service_account.instance_service_account.email}"
+  ]
+}
