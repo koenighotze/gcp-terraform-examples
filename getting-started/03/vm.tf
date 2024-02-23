b0ad39f5ffb171bcef7540a14c9960546224ebe8
koenighotze <david.schmitz.privat@gmail.com>
2024-02-23 10:02:20 +0100

    Setup nginx

--- getting-started/03/vm.tf
+++ getting-started/03/vm.tf
@@ -32,8 +32,6 @@ resource "google_compute_instance" "webserver" {
     }
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
-
   service_account {
     email = google_service_account.instance_service_account.email
     # https://cloud.google.com/sdk/gcloud/reference/alpha/compute/instances/set-scopes#--scopes
@@ -44,6 +42,14 @@ resource "google_compute_instance" "webserver" {
     #checkov:skip=CKV_GCP_32: We will use ssh keys down the line
     block-project-ssh-keys = false
   }
+
+  metadata_startup_script = <<SCRIPT
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+SCRIPT
 
   tags = ["webserver"]
 }
