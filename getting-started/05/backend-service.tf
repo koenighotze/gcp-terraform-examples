e148b41584dad11f0a4460d5ae9870337c7c0358
koenighotze <david.schmitz.privat@gmail.com>
2024-03-07 18:02:52 +0100

    Ex05 LB

--- getting-started/05/backend-service.tf
+++ getting-started/05/backend-service.tf
@@ -4,7 +4,7 @@ resource "google_compute_region_backend_service" "backend_service" {
   protocol              = "HTTP"
   port_name             = "http"
   timeout_sec           = 10
-  health_checks         = [google_compute_health_check.health_check.self_link]
+  health_checks         = [google_compute_region_health_check.health_check.self_link]
   session_affinity      = "NONE"
   load_balancing_scheme = "EXTERNAL"
 
@@ -13,7 +13,7 @@ resource "google_compute_region_backend_service" "backend_service" {
   }
 }
 
-resource "google_compute_health_check" "health_check" {
+resource "google_compute_region_health_check" "health_check" {
   name               = "http-health-check-${local.name_postfix}"
   check_interval_sec = 5
   timeout_sec        = 5
