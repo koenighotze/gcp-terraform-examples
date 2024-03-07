0f62219af01e8711deaa05d12c0b9683cf688dd1
koenighotze <david.schmitz.privat@gmail.com>
2024-03-07 17:04:44 +0100

    Ex05 backend service

---
+++ getting-started/05/backend-service.tf
@@ -0,0 +1,22 @@
+resource "google_compute_backend_service" "backend_service" {
+  name             = "backend-service-${local.name_postfix}"
+  protocol         = "HTTP"
+  port_name        = "http"
+  timeout_sec      = 10
+  health_checks    = [google_compute_health_check.health_check.self_link]
+  session_affinity = "NONE"
+
+  backend {
+    group = google_compute_region_instance_group_manager.manager.self_link
+  }
+}
+
+resource "google_compute_health_check" "health_check" {
+  name               = "http-health-check-${local.name_postfix}"
+  check_interval_sec = 5
+  timeout_sec        = 5
+
+  http_health_check {
+    port_name = "http"
+  }
+}
