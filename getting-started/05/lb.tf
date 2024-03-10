1c5270e21ff1e7c4893dad497fc507117438c5c9
koenighotze <david.schmitz.privat@gmail.com>
2024-03-10 17:59:28 +0100

    Setup load balancer

--- getting-started/05/lb.tf
+++ getting-started/05/lb.tf
@@ -1,9 +1,11 @@
-# resource "google_compute_forwarding_rule" "forwarding_rule" {
-#   name                  = "forwarding-rule-${local.name_postfix}"
-#   load_balancing_scheme = "EXTERNAL_MANAGED"
-#   network_tier          = "STANDARD"
-#   target                = google_compute_region_target_http_proxy.http_proxy.self_link
-# }
+resource "google_compute_forwarding_rule" "forwarding_rule" {
+  name                  = "forwarding-rule-${local.name_postfix}"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  network_tier          = "STANDARD"
+  port_range            = 80
+  network               = google_compute_network.vpc.id
+  target                = google_compute_region_target_http_proxy.http_proxy.self_link
+}
 
 resource "google_compute_region_target_http_proxy" "http_proxy" {
   name    = "http-proxy-${local.name_postfix}"
