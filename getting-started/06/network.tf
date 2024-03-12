d077e51dc81e5a73ae4b308fc7eb2396c5120d51
koenighotze <david.schmitz.privat@gmail.com>
2024-03-12 22:07:40 +0100

    Setup network

--- getting-started/06/network.tf
+++ getting-started/06/network.tf
@@ -8,19 +8,31 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = false
 }
 
+#checkov:skip=CKV_GCP_76: no priv google access
 resource "google_compute_subnetwork" "instance_subnetwork" {
-  name          = "instance-subnetwork-${local.name_postfix}"
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = "10.1.0.0/23"
+  name                     = "instance-subnetwork-${local.name_postfix}"
+  network                  = google_compute_network.vpc.id
+  ip_cidr_range            = "10.1.0.0/23"
+  stack_type               = "IPV4_ONLY"
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
+
 resource "google_compute_firewall" "firewall" {
-  name          = "allow-ssh-http"
-  network       = google_compute_network.vpc.id
-  direction     = "INGRESS"
-  priority      = 1000
+  name      = "allow-ssh-http"
+  network   = google_compute_network.vpc.id
+  direction = "INGRESS"
+  priority  = 1000
+  #checkov:skip=CKV_GCP_106:allow everybody to connect
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["webserver"]
+  #checkov:skip=CKV_GCP_2: we need ssh and http
   allow {
     protocol = "tcp"
     ports    = ["80", "22"]
