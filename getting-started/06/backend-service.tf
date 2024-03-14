9ad528aa08551645c20636d5deacd2cc8cda7b65
David Schmitz <david.schmitz.privat@gmail.com>
2024-03-14 10:47:15 +0100

    Add backend service

--- getting-started/06/backend-service.tf
+++ getting-started/06/backend-service.tf
@@ -1,3 +1,35 @@
 
 # google_compute_region_backend_service
 # google_compute_http_health_check
+
+
+resource "google_compute_region_backend_service" "backend_service" {
+  name                  = "backend-service-${local.name_postfix}"
+  protocol              = "HTTP"
+  port_name             = "http"
+  health_checks         = [google_compute_region_health_check.http_health_check.id]
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  network               = google_compute_network.vpc.id
+  backend {
+    group           = google_compute_region_instance_group_manager.mig.id
+    balancing_mode  = "RATE"
+    max_rate        = 100
+    capacity_scaler = 1.0
+  }
+
+  log_config {
+    enable      = true
+    sample_rate = 1.0
+  }
+}
+
+resource "google_compute_region_health_check" "http_health_check" {
+  name = "http-health-check-${local.name_postfix}"
+  http_health_check {
+    port_name          = "http"
+    port_specification = "USE_NAMED_PORT"
+  }
+  log_config {
+    enable = true
+  }
+}
